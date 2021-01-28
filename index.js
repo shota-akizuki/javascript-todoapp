3fbed0effad31baaca0b70e34257977ce82c3dc3
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-01-29 02:31:05 +0900

    Todoアイテムの追加を実装する

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-console.log("index.js loaded");
-
 import { App } from "./src/App.js";
 
 const app = new App();
+app.mount();
--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-console.log("App.js loaded");
+import { element } from "./view/html-util.js";
 
 export class App {
-  constructor() {
-    console.log("App initialized");
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    // Todoアイテム数
+    let todoItemCount = 0;
+    formElement.addEventListener("submit", (event) => {
+      // preventDefaultしないとページがリロードされてしまう
+      // submitイベントの本来の動作を止める
+      event.preventDefault();
+      // 追加するTodoアイテムの要素(li要素)を作成する
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      // Todoアイテムをcontainerに追加する
+      containerElement.appendChild(todoItemElement);
+      // Todoアイテム数を+1し、表示されてるテキストを更新する
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      // 入力欄を空文字列にしてリセットする
+      inputElement.value = "";
+    });
   }
 }
